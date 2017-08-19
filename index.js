c0601e8c438f797bbaedebbc6ffafb3b01de61d8
inuscript <inuscript@users.noreply.github.com>
2017-08-19 13:00:52 +0900

    cotton candy founder

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+const assert = require("assert")
 const puppeteer = require('puppeteer');
 
 const getFlavorPage = () => {
   return "https://www.31ice.co.jp/contents/product/flavor/index.html"
 }
 
+const hasCottonFlavor = (flavors) => {
+  flavors.map(flavor => {
+    assert(!/.+コットン.+/.test(flavor), `${flavor} found`)
+  })
+}
 
 (async () => {
   
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage()
   await page.goto(getFlavorPage())
   const result = await page.evaluate(() => {
@@ -19,11 +25,13 @@ const getFlavorPage = () => {
     
     const seasonFlavors = getFravors("#seasonFlavorPhoto a img")
     const standardFlavors = getFravors("#standardFlavorPhoto a img")
-    return Promise.resolve({
-      seasonFlavors, standardFlavors
-    })
+    return Promise.resolve([
+      ...seasonFlavors, ...standardFlavors
+    ])
   })
-  console.log(result)
-
+  hasCottonFlavor(result)
   browser.close();
-})();
+})().catch(e => {
+  console.log(e.toString())
+  process.exit(1)
+})
